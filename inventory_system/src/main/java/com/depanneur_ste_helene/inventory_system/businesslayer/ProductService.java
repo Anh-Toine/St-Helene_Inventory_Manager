b4bee78992bc6b623622a55ea98bda26e3accad8
jumpung <58006159+jumpung@users.noreply.github.com>
2021-12-07 19:01:13 -0500

    Add files via upload

---
+++ inventory_system/src/main/java/com/depanneur_ste_helene/inventory_system/businesslayer/ProductService.java
@@ -0,0 +1,8 @@
+package com.depanneur_ste_helene.inventory_system.businesslayer;
+
+import com.depanneur_ste_helene.inventory_system.datalayer.Product;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface ProductService {
+    Product CreateProduct(@RequestBody Product model);
+}
---
+++ inventory_system/src/test/java/com/depanneur_ste_helene/inventory_system/InventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depanneur_ste_helene.inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
