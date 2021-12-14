ddceba37658426ccba1c18635a48e2dbd6625e73
Dratorz <chhunrandy02@gmail.com>
2021-12-14 13:18:29 -0500

    Added base service for getAllProduct

--- src/main/java/com/depanneur_ste_helene/inventory_system/businesslayer/ProductServiceImpl.java
+++ src/main/java/com/depanneur_ste_helene/inventory_system/businesslayer/ProductServiceImpl.java
@@ -3,7 +3,13 @@ package com.depanneur_ste_helene.inventory_system.businesslayer;
 import com.depanneur_ste_helene.inventory_system.datalayer.Product;
 import com.depanneur_ste_helene.inventory_system.datalayer.ProductRepository;
 import org.springframework.stereotype.Service;
+<<<<<<< HEAD
 import com.depanneur_ste_helene.inventory_system.exceptions.InvalidInputException;
+=======
+
+import java.util.List;
+
+>>>>>>> 33602aa (Added base service for getAllProduct)
 @Service
 public class ProductServiceImpl implements ProductService{
 
@@ -13,11 +19,16 @@ public class ProductServiceImpl implements ProductService{
         this.productRepository = productRepository;
     }
 
-    @Override
     public Product createProduct(Product model) {
         if(model.getPrice() < 0 || model.getQuantity() < 0 || model.getQuantity_sold() < 0){
             throw new InvalidInputException("Input not valid");
         }
+
         return productRepository.save(model);
+    }
+
+    public List<Product> getAllProduct() {
+        List<Product> products = productRepository.findAll();
+        return products;
     }
 }
