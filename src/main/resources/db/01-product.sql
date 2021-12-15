f0571133fe7118c5892d6f7c330e6a75e0f7d9b8
Brandon Thorng <brandonthorng@gmail.com>
2021-12-15 15:19:16 -0500

    Snake case ssssssssssssssssssssssssssssssssssssssssssssssss

--- src/main/resources/db/01-product.sql
+++ src/main/resources/db/01-product.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS product;
 
 CREATE TABLE IF NOT EXISTS product (
-    productId INT(4) AUTO_INCREMENT PRIMARY KEY,
-    barCode INT(100),
+    product_id INT(4) AUTO_INCREMENT PRIMARY KEY,
+    bar_code INT(100),
     brand VARCHAR(255),
-    categoryId INT(100),
+    category_id INT(100),
     price DOUBLE,
-    productName VARCHAR(255),
+    product_name VARCHAR(255),
     quantity INT(100),
-    quantitySold INT(100)
+    quantity_sold INT(100)
 ) engine=InnoDB;
