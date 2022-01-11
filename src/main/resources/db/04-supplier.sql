714dfff29f0fc707fb15a6d12607de0e22f985ef
Anh-Toine <Nick1234567890a@gmail.com>
2022-01-11 18:22:17 -0500

    Small edits to the SQL files for suppliers.

--- src/main/resources/db/04-supplier.sql
+++ src/main/resources/db/04-supplier.sql
@@ -1,4 +1,5 @@
-INSERT INTO supplier(supplier_name, representative_name, email, phone_number)
-VALUES ('The Coca-Cola Company','James R. Enricho','james_encriko@protonmail.com','111-1111-1111');
-INSERT INTO supplier(supplier_name, representative_name, email, phone_number)
-VALUES ('Unilever','Alecia F. Zaramophska','alecia11288@gmail.com','222-2222-2222');
+INSERT IGNORE INTO supplier VALUES (1,'The Coca-Cola Company','James R. Enricho',
+    'james_encriko@protonmail
+.com','111-1111-1111');
+INSERT IGNORE INTO supplier VALUES (2,'Unilever','Alecia F. Zaramophska','alecia11288@gmail.com',
+                             '222-2222-2222');
