4ee06d54b1369aae4d13df7fae3055a6fcfb9d01
Anh-Toine <Nick1234567890a@gmail.com>
2022-01-12 09:11:37 -0500

    Added UNIQUE constraints to fields supplier_name, email, and phone_number to the supplier table.

--- src/main/resources/db/03-supplier.sql
+++ src/main/resources/db/03-supplier.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS supplier;
 
 CREATE TABLE IF NOT EXISTS supplier(
     supplier_id INT PRIMARY KEY AUTO_INCREMENT,
-    supplier_name VARCHAR(255),
+    supplier_name VARCHAR(255) UNIQUE,
     representative_name VARCHAR(255),
-    email VARCHAR(255),
-    phone_number VARCHAR(255)
+    email VARCHAR(255) UNIQUE,
+    phone_number VARCHAR(255) UNIQUE
 )engine=InnoDB;
 
 
