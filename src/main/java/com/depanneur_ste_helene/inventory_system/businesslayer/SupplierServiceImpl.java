394319800fbfc4d4e6d9dc63f8e1e7621d4cdf27
Anh-Toine <Nick1234567890a@gmail.com>
2022-01-12 09:52:27 -0500

    Working on createSupplier use case

--- src/main/java/com/depanneur_ste_helene/inventory_system/businesslayer/SupplierServiceImpl.java
+++ src/main/java/com/depanneur_ste_helene/inventory_system/businesslayer/SupplierServiceImpl.java
@@ -3,6 +3,7 @@ package com.depanneur_ste_helene.inventory_system.businesslayer;
 import com.depanneur_ste_helene.inventory_system.datalayer.Supplier;
 import com.depanneur_ste_helene.inventory_system.datalayer.SupplierDTO;
 import com.depanneur_ste_helene.inventory_system.datalayer.SupplierRepository;
+import com.depanneur_ste_helene.inventory_system.exceptions.InvalidInputException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,5 +23,16 @@ public class SupplierServiceImpl implements SupplierService{
        List<Supplier> suppliers = supplierRespository.findAll();
        List<SupplierDTO> models = supplierMapper.entityListToModelList(suppliers);
        return models;
+    }
+
+    @Override
+    public SupplierDTO createSupplier(SupplierDTO supplier) {
+        if(supplier.getSupplierName().equals("") ||
+                supplier.getRepresentativeName().equals("") ||
+                supplier.getEmail().equals("") ||
+                supplier.getPhoneNumber().equals("")){
+            throw new InvalidInputException("Input not valid");
+        }
+        return null;
     }
 }
