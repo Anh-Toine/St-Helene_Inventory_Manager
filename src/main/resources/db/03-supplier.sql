273213a3a99de1941a6c77913840dab8e5b8a62e
Anh-Toine <Nick1234567890a@gmail.com>
2022-01-18 14:36:34 -0500

    Removed redundant UNIQUE constraints in the supplier table.

--- src/main/resources/db/03-supplier.sql
+++ src/main/resources/db/03-supplier.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS supplier(
     supplier_id INT PRIMARY KEY AUTO_INCREMENT,
     supplier_name VARCHAR(255) UNIQUE,
     representative_name VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    phone_number VARCHAR(255) UNIQUE
+    email VARCHAR(255),
+    phone_number VARCHAR(255)
 )engine=InnoDB;
 
 
