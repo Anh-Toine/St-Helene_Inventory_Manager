efa537e9fbed2c88b2af1cb8298bf9e2dc46d65d
Anh-Toine <Nick1234567890a@gmail.com>
2022-01-27 09:56:30 -0500

    View orders should be working now.

--- src/main/java/com/depanneur_ste_helene/inventory_system/datalayer/order/Order.java
+++ src/main/java/com/depanneur_ste_helene/inventory_system/datalayer/order/Order.java
@@ -5,7 +5,7 @@ import java.sql.Date;
 import java.util.UUID;
 
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
