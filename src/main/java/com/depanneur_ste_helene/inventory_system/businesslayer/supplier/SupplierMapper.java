25afbe28f31d358e27733ce1948dd0b076703a7b
Hathenia <Nick1234567890a@gmail.com>
2022-02-12 12:17:38 -0500

    Added new createDTOToEntity method.

--- src/main/java/com/depanneur_ste_helene/inventory_system/businesslayer/supplier/SupplierMapper.java
+++ src/main/java/com/depanneur_ste_helene/inventory_system/businesslayer/supplier/SupplierMapper.java
@@ -1,15 +1,23 @@
 package com.depanneur_ste_helene.inventory_system.businesslayer.supplier;
 
 import com.depanneur_ste_helene.inventory_system.datalayer.supplier.Supplier;
+import com.depanneur_ste_helene.inventory_system.datalayer.supplier.SupplierCreateDTO;
 import com.depanneur_ste_helene.inventory_system.datalayer.supplier.SupplierDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 import java.util.UUID;
 
 @Mapper(componentModel = "spring", imports = {UUID.class})
 public interface SupplierMapper {
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "supplierId", ignore = true)
+    })
+    Supplier createDTOToEntity(SupplierCreateDTO model);
 
     SupplierDTO entityToModel(Supplier entity);
     List<SupplierDTO> entityListToModelList(List<Supplier> entities);
